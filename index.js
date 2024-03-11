d11e920adb710cc776da7ac4e1d61ca5dcb2254a
mqhelem <155308250+mqhelem@users.noreply.github.com>
2024-03-11 23:29:38 +0200

    updates

--- index.js
+++ index.js
@@ -49,6 +49,32 @@ function changeDate(date) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+// forecast
+function displayForecast() {
+  let weatherForecast = document.querySelector("#forecast");
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="row">
+    <div class="c0l-2">
+      <div class="forecast-day">${day}</div>
+      <img
+        src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+        alt=""
+      />
+      <div class="weather-temperature">
+        <span class="max-temperature">18°</span>
+        <span class="min-temperature">12°</span>
+      </div>
+    </div>
+  </div>
+`;
+  });
+  weatherForecast.innerHTML = forecastHTML;
+}
+displayForecast();
 let searchForm = document.querySelector("#searchForm");
 searchForm.addEventListener("submit", handleSubmit);
 searchCity("Paris");
